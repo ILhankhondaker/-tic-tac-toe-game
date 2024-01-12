0226d1dd42a6774088683d4435a8f7df031042a5
ILhankhondaker <ilhankhondaker@gmail.com>
2024-01-13 03:17:01 +0600

    push all code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Game from "./Component/Game";
+import Start from "./Component/Start";
+
+function App() {
+  return (
+    <>
+      <Start></Start>
+      <Game></Game>
+    </>
+  );
+}
+
+export default App;
